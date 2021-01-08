9fe77190d03d0de2d7ad2b62051b5ba877d47885
Utilka <andrii.v.bolshakov@gmail.com>
2021-01-08 21:13:56 +0200

    beautify + db beautify

--- bin/database.js
+++ bin/database.js
@@ -1,16 +1,16 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('../MAIN');
+var db = new sqlite3.Database('./db_data/main.sqlite3');
 
 db.serialize(function() {
 
-  // db.run('CREATE TABLE lorem (info TEXT)');
-  // var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-
-  // for (var i = 0; i < 10; i++) {
-  //   stmt.run('Ipsum ' + i);
-  // }
-
-  // stmt.finalize();
+//  db.run('CREATE TABLE lorem (info TEXT)');
+//  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+//
+//  for (var i = 0; i < 10; i++) {
+//    stmt.run('Ipsum ' + i);
+//  }
+//
+//  stmt.finalize();
 
   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
     console.log(row.id + ': ' + row.info);
