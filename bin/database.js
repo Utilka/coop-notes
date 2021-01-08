20f6cabc72901ac7f91d79fac2a4eb26be2222dc
Yurii Agafonov <storminside4766@gmail.com>
2021-01-08 22:59:29 +0200

    try pug

--- bin/database.js
+++ bin/database.js
@@ -3,14 +3,14 @@ var db = new sqlite3.Database('./db_data/main.sqlite3');
 
 db.serialize(function() {
 
-//  db.run('CREATE TABLE lorem (info TEXT)');
-//  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-//
-//  for (var i = 0; i < 10; i++) {
-//    stmt.run('Ipsum ' + i);
-//  }
-//
-//  stmt.finalize();
+ db.run('CREATE TABLE lorem (info TEXT)');
+ var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+
+ for (var i = 0; i < 10; i++) {
+   stmt.run('Ipsum ' + i);
+ }
+
+ stmt.finalize();
 
   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
     console.log(row.id + ': ' + row.info);
