949f7e890b8a0947ef25b5d49ecf1fcc6e2c72b4
Utilka <andrii.v.bolshakov@gmail.com>
2021-01-09 12:34:49 +0200

    started db init func

--- bin/database.js
+++ bin/database.js
@@ -1,39 +1,50 @@
 var sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database('./db_data/main.sqlite3')
 
-function createTables(){
+function db_init() {
+    create_table("lorem","(info TEXT)" )
+    create_table("loo","(boo TEXT)" )
+    create_table("joo","(foo TEXT)" )
 
-  db.serialize(function() {
+}
+function create_table(table_name,parameters) {
+    console.log(`create_table: ${table_name} with ${parameters}`);
+    db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='${table_name}'`, [],
+        function (err, row) {
+            console.log(`table "${table_name}" exists: ${row !== undefined}`);
+            if (row === undefined){
+                db.run(`CREATE TABLE ${table_name} ${parameters}`,  [], function (err) {throw err})
+                console.log(`created table: ${table_name} ${parameters} ////`);
+            } // if database did not returned a row with table
+        })
+}
 
-    db.get("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='lorem'", function(err, row){
-      if (row['count(name)'] != 1){
-        console.log("Ы");
-        console.info("True")
-        // db.run('CREATE TABLE ${tables[i]} (info (?))')
-      }
-    })
+function end_work() {
+    db.close();
+}
 
-  })
+db.serialize(function () {
 
-  db.close();
-};
+    // db.run('CREATE TABLE lorem (info TEXT)')
+    // var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+    //
+    // for (var i = 0; i < 10; i++) {
+    //     stmt.run('Ipsum ' + i)
+    // }
+    //
+    // stmt.finalize()
+    //
+    // db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+    //     console.log(row.id + ': ' + row.info);
+    // })
+})
 
-// db.serialize(function() {
 
-//  db.run('CREATE TABLE lorem (info TEXT)')
-//  var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+db_init()
+end_work()
 
-//  for (var i = 0; i < 10; i++) {
-//    stmt.run('Ipsum ' + i)
-//  }
-
-//  stmt.finalize()
-
-//   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-//     console.log(row.id + ': ' + row.info);
-//   })
-// })
-
-// db.close()
-
-module.exports.createTables = createTables;
+// module.exports = {
+//     db : db,
+//     check_table_exist: check_table_exist,
+//     end_work:end_work
+// }
