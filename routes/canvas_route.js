042d9e2d1592b8fa80957295632a5b401175abf1
Utilka <andrii.v.bolshakov@gmail.com>
2021-01-10 23:20:42 +0200

    beautify

--- routes/canvas_route.js
+++ routes/canvas_route.js
@@ -3,19 +3,14 @@ const { valuesIn } = require('lodash');
 var router = express.Router();
 var op = require("../bin/operations.js")
 
-
-
 var canvList = op.canvList
 
-
-
-router.get('/:cTitle', function(req, res, next) {
-    var canvasTitle = req.params['canvasTitle']
-    var Canvas = canvList.find(x => x.title === canvasTitle)
-  return res.render('canvas', {
-     Canvas: Canvas,
-     
-  });
-});
+router.get('/:canvasTitle', function(req, res, next) {
+    let canvasTitle = req.params['canvasTitle']
+    let canvas = canvList.find(x => x.title === canvasTitle)
+    return res.render('canvas', {
+        canvas: canvas,
+    })
+})
 
 module.exports = router;
