4710ef0b37f30b9f2578a18ac5aaca7365dc3ba8
Utilka <andrii.v.bolshakov@gmail.com>
2021-01-13 16:06:25 +0200

    promisify db init and write example objects

--- bin/database.js
+++ bin/database.js
@@ -1,28 +1,80 @@
 var sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database('./db_data/main.sqlite3')
 
-function db_init(callback) {
-    create_table("lorem","(info TEXT)" )
-    create_table("poo","(boo TEXT)" )
-    create_table("koo","(foo TEXT)" )
-    callback()
+function db_init() {
+    return new Promise(function (resolve, reject) {
+        check_table_exist("lorem").then(create_table_constr("lorem", "(info TEXT)"))
+        check_table_exist("foo").then(create_table_constr("foo", "(boo TEXT)"))
+        resolve()
+    })
 }
 
-function create_table(table_name,parameters) {
-    console.log(`create_table: ${table_name} with ${parameters}`);
-    db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='${table_name}'`, [],
-        function (err, row) {
-            // if (err){throw err}
-            console.log(`table "${table_name}" exists: ${row !== undefined}`);
-            if (row === undefined){
-                db.run(`CREATE TABLE ${table_name} ${parameters}`,  [], function (err) { if (err){throw err}})
-                console.log(`created table: ${table_name} ${parameters} ////`);
-            } // if database did not returned a row with table
-        })
+// function create_table(table_name, parameters) {
+//     console.log(`create_table: ${table_name} with ${parameters}`);
+//     db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='${table_name}'`, [],
+//         function (err, row) {
+//             // if (err){throw err}
+//             console.log(`table "${table_name}" exists: ${row !== undefined}`);
+//             if (row === undefined) {
+//                 db.run(`CREATE TABLE ${table_name} ${parameters}`, [], function (err) {
+//                     if (err) {
+//                         throw err
+//                     }
+//                 })
+//                 console.log(`created table: ${table_name} ${parameters} ////`);
+//             } // if database did not returned a row with table
+//         })
+// }
+
+function check_table_exist(table_name) {
+    return new Promise(function (resolve, reject) {
+        db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='${table_name}'`, [],
+            function (err, row) {
+                console.log(`table ${table_name} exist: ${row !== undefined}`);
+                if (err) {
+                    reject(err)
+                }
+                if (row !== undefined) {
+                    resolve(row.name)
+                } else {
+                    resolve(undefined)
+                }
+            })
+    })
+}
+
+// function create_table(table_name, parameters) {
+//     return new Promise(function (resolve, reject) {
+//         db.run(`CREATE TABLE ${table_name} ${parameters}`, [], function (err) {
+//             if (err) {
+//                 reject(err)
+//             }
+//             else {
+//                 console.log(`created table: ${table_name} ${parameters}`)
+//                 resolve()
+//             }
+//         })
+//     })
+// }
+
+function create_table_constr(table_name, parameters) {
+    return function (table_exists) {
+        if (table_exists === undefined) {
+            db.run(`CREATE TABLE ${table_name} ${parameters}`, [], function (err) {
+                if (err) {
+                    throw err
+                } else {
+                    console.log(`created table: ${table_name} ${parameters}`)
+                }
+            })
+        }
+
+    }
 }
 
 function end_work() {
     db.close();
+    console.log("database closed")
 }
 
 db.serialize(function () {
@@ -41,10 +93,9 @@ db.serialize(function () {
     // })
 })
 
-// db_init(end_work)
-
 module.exports = {
-    db_init: db_init,
-    end_work:end_work
+    "db": db,
+    "db_init": db_init,
+    "end_work": end_work
 }
 
--- bin/operations.js
+++ bin/operations.js
@@ -1,4 +1,7 @@
 var db = require('./database')
+var User = require("../lib/User.js")
+var Note = require("../lib/Note.js")
+var Connection = require("../lib/Connection.js")
 var Canvas = require("../lib/Canvas.js")
 
 
@@ -6,16 +9,120 @@ var Canvas = require("../lib/Canvas.js")
  * Working with Database.
  */
 
- db.db_init(db.end_work)
-
+// db.db_init(db.end_work)
+db.db_init().then(db.end_work)
 //Check if canvas exist
-    //Create Canvas if no
+//Create Canvas if no
 
 //Get Canvases from db in list
-var canvas1 = new Canvas(1, "Canvas1", "1")
-var canvas2 = new Canvas(2, "Canvas2", "1")
 
-var canvList = [canvas1, canvas2]
+//Temporary data init
+var users = [
+    new User(1, "User Kot"),
+    new User(2, "User Foo"),
+    new User(3, "User ShiZ")
+]
+
+var canv = [
+    new Canvas(1, "Canvas1", users[0]),
+    new Canvas(2, "Canvas2", users[0]),
+    new Canvas(3, "Canvas3", users[1])
+]
+
+var notes0 = [
+    new Note(1, "Note1", users[0], [10, 100]),
+    new Note(2, "Note2", users[0], [500, 100]),
+    new Note(3, "Note3", users[1], [1000, 500]),
+    new Note(4, "Note4", users[1], [500, 500])
+]
+canv[0].notes = notes0
+
+var notes1 = [
+    new Note(5, "Note5", users[0], [200, 100]),
+    new Note(6, "Note6", users[0], [-200, 100]),
+    new Note(7, "Note7", users[2], [0, -200]),
+    new Note(8, "Note8", users[2], [-100, -400])
+]
+canv[1].notes = notes1
+
+var notes2 = [
+    new Note(9, "Note9", users[1], [-200, -200]),
+    new Note(10, "Note10", users[1], [200, -200]),
+    new Note(11, "Note11", users[2], [200, 200]),
+    new Note(12, "Note12", users[2], [-200, 200])
+]
+canv[2].notes = notes2
+
+var connections0 = [
+    new Connection(1, users[0],
+        notes1[0], "note", "auto", undefined,
+        notes1[1], "note", "auto", undefined,
+    ),
+    new Connection(2, users[1],
+        notes1[0], "note", "auto", undefined,
+        notes1[2], "note", "auto", undefined,
+    ),
+    new Connection(3, users[1],
+        notes1[1], "note", "auto", undefined,
+        notes1[3], "note", "auto", undefined,
+    ),
+    new Connection(4, users[1],
+        notes1[1], "note", "auto", undefined,
+        notes1[2], "note", "auto", undefined,
+    ),
+    new Connection(5, users[1],
+        notes1[2], "note", "auto", undefined,
+        notes1[3], "note", "auto", undefined,
+    )
+]
+canv[0].connections = connections0
+
+var connections1 = [
+    new Connection(6, users[0],
+        notes2[0], "note", "auto", undefined,
+        notes2[2], "note", "auto", undefined,
+    ),
+    new Connection(7, users[0],
+        notes2[2], "note", "auto", undefined,
+        notes2[3], "note", "auto", undefined,
+    ),
+]
+connections1[2] = new Connection(8, users[2],
+    "note", notes2[2], "auto", undefined,
+    "connection", connections1[1], "auto", undefined,
+)
+canv[1].connections = connections1
+
+var connections2 = [
+    new Connection(9, users[0],
+        notes2[0], "note", "auto", undefined,
+        notes2[1], "note", "auto", undefined,
+    ),
+    new Connection(10, users[1],
+        notes2[1], "note", "auto", undefined,
+        undefined, "free", "absolute", [300, -300],
+    ),
+    new Connection(11, users[3],
+        undefined, "free", "absolute", [-300, -300],
+        notes2[0], "note", "auto", undefined,
+    ),
+    new Connection(12, users[2],
+        notes2[2], "note", "auto", undefined,
+        notes2[3], "note", "auto", undefined,
+    ),
+    new Connection(13, users[3],
+        notes2[3], "note", "auto", undefined,
+        undefined, "free", "absolute", [-300, 300],
+    ),
+    new Connection(14, users[3],
+        undefined, "free", "absolute", [300, 300],
+        notes2[2], "note", "auto", undefined,
+    )
+]
+
+canv[2].connections = connections2
+
+var canvList = canv
 
 
 // for(var i=0;i<canvList.length;i++){
