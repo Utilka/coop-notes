b31e456d48717ef599d816f8734835a4db2047e2
Utilka <andrii.v.bolshakov@gmail.com>
2021-01-28 15:29:23 +0200

    final tweaks before some risky coding

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,19 +35,23 @@ app.use('/canvas', canvasRouter);
 app.use('/data', dataRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    if ((req.app.get('env') === 'development') || (req.app.get('env') === 'testing')) {
+        res.locals.error = err;
+    } else {
+        res.locals.error = {};
+    }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
