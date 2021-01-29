3645f436a14ac46959882a0e15c4727ec13e1148
Vanya <memlord1@gmail.com>
2021-01-29 13:03:49 +0200

    add new sran'

--- routes/data_interaction.js
+++ routes/data_interaction.js
@@ -7,9 +7,9 @@ router.get('/', (req, res) => {
     res.json(global.gConfig);
 });
 router.get('/get_user', (req, res) => {
-    let id = parseInt(req.query.id, 10)  // true
+    let id = parseInt(req.query.id, 10);
     db.di.get_user(id).then(function (user) {
-        res.json(user)
+        res.json(user);
     }).catch(function (reason) {
             console.log(reason);
             res.status(505).send(reason);
@@ -18,9 +18,9 @@ router.get('/get_user', (req, res) => {
 });
 router.get('/get_canvas', (req, res) => {
     //GET /data/get_canvas?id_canvas=1
-    let id_canvas = parseInt(req.query.id_canvas, 10);  // true
+    let id_canvas = parseInt(req.query.id_canvas, 10); 
     db.di.get_canvas(id_canvas).then(function (user) {
-        res.json(user)
+        res.json(user);
     }).catch(function (reason) {
         console.log(reason);
         res.status(505).send(reason);
@@ -30,9 +30,9 @@ router.get('/get_canvas', (req, res) => {
 router.get('/get_canvas_permission', (req, res) => {
     //GET /data/get_canvas_permission?id_user=1&id_canvas=3
     let id_user = parseInt(req.query.id_user, 10);
-    let id_canvas = parseInt(req.query.id_canvas, 10);  // true
+    let id_canvas = parseInt(req.query.id_canvas, 10); 
     db.di.get_canvas_permission(id_user, id_canvas).then(function (row) {
-        res.json(row)
+        res.json(row);
     }).catch(function (reason) {
         console.log(reason);
         res.status(505).send(reason);
@@ -42,7 +42,7 @@ router.get('/get_note', (req, res) => {
     //GET /data/get_note?note_id=1
     let note_id = parseInt(req.query.note_id, 10);
     db.di.get_note(note_id).then(function (note) {
-        res.json(note)
+        res.json(note);
     }).catch(function (reason) {
         console.log(reason);
         res.status(505).send(reason);
@@ -52,7 +52,7 @@ router.get('/get_connection', (req, res) => {
     //GET /data/get_connection?connection_id=1
     let connection_id = parseInt(req.query.connection_id, 10);
     db.di.get_connection(connection_id).then(function (connection) {
-        res.json(connection)
+        res.json(connection);
     }).catch(function (reason) {
         console.log(reason);
         res.status(505).send(reason);
@@ -62,14 +62,12 @@ router.get('/get_picture', (req, res) => {
     //GET /data/get_picture?picture_id=1
     let picture_id = parseInt(req.query.picture_id, 10);
     db.di.get_picture(picture_id).then(function (picture) {
-        res.json(picture)
+        res.json(picture);
     }).catch(function (reason) {
         console.log(reason);
         res.status(505).send(reason);
     });
 });
-
-
 router.post('/upd_user', (req, res) => {
     //POST /data/upd_user?user_id=1 body{nick:fill}
     let upd_user_id = parseInt(req.query.user_id, 10);
@@ -104,7 +102,6 @@ router.post('/upd_note', (req, res) => {
         res.status(505).send(reson)
     );
 });
-//---------------------------------------------
 router.post('/upd_connection', (req, res) => {
     //POST /data/upd_connection?conn_id=1 body{title:new Title1}
     let upd_conection_id = parseInt(req.query.conn_id, 10);
@@ -115,7 +112,6 @@ router.post('/upd_connection', (req, res) => {
         res.status(505).send(reson)
     );
 });
-//--------------------------------------------
 router.post('/upd_picture', (req, res) => {
     //POST /data/upd_picture?picture_id=1 body{title:new Title1}
     let upd_picture_id = parseInt(req.query.picture_id, 10);
@@ -124,56 +120,95 @@ router.post('/upd_picture', (req, res) => {
         res.status(505).send(reson)
     );
 });
-//-------------------------------------------
-router.post('/get_user_by_nick', (req, res) => {
-    //POST /data/get_user_by_nick body{nick:andrey}
-    let user_nick = req.body.nick;
-    db.di.get_user_by_nick(user_nick).then(function(user) {res.json(user)}).catch(function (reason) {
-        console.log(reason)
+router.get('/get_user_by_nick', (req, res) => {
+    //GET /data/get_user_by_nick?nick=fill
+    let user_nick = req.query.nick;
+    db.di.get_user_by_nick(user_nick).then(function(user) 
+    {
+        res.json(user);
+    }).catch(function (reason) {
+        console.log(reason);
+        res.status(505).send(reason);
     });
 });
-router.post('/get_permitted_canvasses', (req, res) => {
-    //POST /data/get_permitted_canvasses body{user_id:1}
-    let user_id = req.body.user_id;
-    db.di.get_permitted_canvasses(user_id).then(function(canvas) {res.json(canvas)}).catch(function (reason) {
-        console.log(reason)
+//problems with answer(not what i waited)
+router.get('/get_permitted_canvasses', (req, res) => {
+    //GET /data/get_permitted_canvasses?user_id=1
+    let user_id = parseInt(req.query.user_id, 10);
+    db.di.get_permitted_canvasses(user_id).then(function(canvas)
+     {
+        res.json(canvas);
+    }).catch(function (reason) {
+        console.log(reason);
+        res.status(505).send(reason);
     });
 });
 //TODO
-router.post('/get_canvasses_list', (req, res) => {
-    //POST /data/get_permitted_canvasses body{id:1}
-    let canvas_id = req.body.id;
-    db.di.get_canvasses_list(canvas_id).then(function(canvas) {res.json(canvas)}).catch(function (reason) {
+router.get('/get_canvasses_list', (req, res) => {
+    //GET /data/get_canvasses_list?list=1,2
+    let canvas_id = req.query.list;
+    let canvas_id_list = canvas_id.split(',').map(function(item) {
+        return parseInt(item, 10);
+    });
+    db.di.get_canvasses_list(canvas_id_list).then(function(canvas) 
+    {
+        res.json(canvas)
+    }).catch(function (reason) {
+         res.status(505).send(reason)
         console.log(reason)
     });
 });
-router.post('/get_notes_in_canvas', (req, res) => {
-    //POST /data/get_notes_in_canvas body{canvas_id:1}
-    let canvas_id = req.body.canvas_id;
-    db.di.get_notes_in_canvas(canvas_id).then(function(canvas) {res.json(canvas)}).catch(function (reason) {
-        console.log(reason)
+
+router.get('/get_notes_in_canvas', (req, res) => {
+    //GET /data/get_notes_in_canvas?canvas_id=1
+    let canvas_id = req.query.canvas_id;
+    db.di.get_notes_in_canvas(canvas_id).then(function(canvas) 
+    {
+        res.json(canvas);
+    }).catch(function (reason) {
+        console.log(reason);
+        res.status(505).send(reason);
     });
 });
-router.post('/get_connections_in_canvas', (req, res) => {
-    //POST /data/get_notes_in_canvas body{canvas_id:1}
-    let canvas_id = req.body.canvas_id;
-    db.di.get_notes_in_canvas(canvas_id).then(function(canvas) {res.json(canvas)}).catch(function (reason) {
-        console.log(reason)
+router.get('/get_connections_in_canvas', (req, res) => {
+    //GET /data/get_connections_in_canvas?canvas_id=1
+    let canvas_id = req.query.canvas_id;
+    db.di.get_connections_in_canvas(canvas_id).then(function(canvas) 
+    {
+        res.json(canvas);
+    }).catch(function (reason) {
+        console.log(reason);
+        res.status(505).send(reason);
     });
 });
+
 //can't check(no pictures)
-router.post('/get_pictures_in_canvas', (req, res) => {
-    //POST /data/get_pictures_in_canvas body{canvas_id:2}
+router.get('/get_pictures_in_canvas', (req, res) => {
+    //POST /data/get_pictures_in_canvas?canvas_id=2
     let canvas_id = req.body.canvas_id;
-    db.di.get_pictures_in_canvas(canvas_id).then(function(canvas) {res.json(canvas)}).catch(function (reason) {
-        console.log(reason)
+    db.di.get_pictures_in_canvas(canvas_id).then(function(canvas) 
+    {
+        res.json(canvas);
+    }).catch(function (reason) {
+        console.log(reason);
+        res.status(505).send(reason);
     });
 });
-//-------------------------------------------
+
 router.post('/insert_user', (req, res) => {
     //POST /data/insert_user body{nick:vanya}
     let nick = req.body.nick;
+    console.log(nick);
     db.di.insert_user(nick).then(res.status(200).send('User added!')).catch(
+        res.status(505).send(reson)
+    );
+});
+
+router.post('/insert_canvas', (req, res) => {
+    //POST /data/insert_canvas body{title:Title1, owner_id:1}
+    let title = req.body.title;
+    let owner_id = req.body.owner_id;
+    db.di.insert_user(title, owner_id).then(res.status(200).send('Canvas added!')).catch(
         res.status(505).send(reson)
     );
 });
